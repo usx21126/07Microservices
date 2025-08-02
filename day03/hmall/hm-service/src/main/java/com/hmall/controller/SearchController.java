68736105afc8d3d799e8437249068329d42b2628
2746150991 <2746150991@qq.com>
2025-08-02 12:49:10 +0800

    day03 init commit

---
+++ day03/hmall/hm-service/src/main/java/com/hmall/controller/SearchController.java
@@ -0,0 +1,32 @@
+package com.hmall.controller;
+
+
+import cn.hutool.core.util.StrUtil;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.hmall.common.domain.PageDTO;
+import com.hmall.domain.dto.ItemDTO;
+import com.hmall.domain.po.Item;
+import com.hmall.domain.query.ItemPageQuery;
+import com.hmall.service.IItemService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(tags = "搜索相关接口")
+@RestController
+@RequestMapping("/search")
+@RequiredArgsConstructor
+public class SearchController {
+
+    private final IItemService itemService;
+
+    @ApiOperation("搜索商品")
+    @GetMapping("/list")
+    public PageDTO<ItemDTO> search(ItemPageQuery query) {
+        PageDTO<ItemDTO> pageDTO = itemService.search(query);
+        return pageDTO;
+    }
+}
---
+++ day03/hmall/hm-service/src/main/java/com/hmall/service/IItemService.java
@@ -0,0 +1,28 @@
+package com.hmall.service;
+
+import com.hmall.common.domain.PageDTO;
+import com.hmall.domain.dto.ItemDTO;
+import com.hmall.domain.dto.OrderDetailDTO;
+import com.hmall.domain.po.Item;
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.hmall.domain.query.ItemPageQuery;
+
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * <p>
+ * 商品表 服务类
+ * </p>
+ *
+ * @author itheima
+ * @since 2023-05-05
+ */
+public interface IItemService extends IService<Item> {
+
+    void deductStock(List<OrderDetailDTO> items);
+
+    List<ItemDTO> queryItemByIds(Collection<Long> ids);
+
+    PageDTO<ItemDTO> search(ItemPageQuery query);
+}
